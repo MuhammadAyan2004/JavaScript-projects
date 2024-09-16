07ebb1eefb33c28dcc115cbdcbda677126749cf3
Muhammad AYAN <m101ayan92@gmail.com>
2024-09-16 23:48:48 +0500

    Update script.js

--- daily quote/script.js
+++ daily quote/script.js
@@ -2,7 +2,21 @@ const quotes = document.getElementById("quotes");
 const authors = document.getElementById("author");
 const newBtn = document.getElementById("new");
 const tweet = document.getElementById("tweet");
-const api_url = "https://api.api-ninjas.com/v1/quotes?";
+const menu = document.getElementById("menu");
+const list = document.getElementById("list");
+
+let selectedCategory = '';
+
+function menuItem(){
+    list.style.display="block"; 
+}
+function listItem(e){
+    selectedCategory = e.target.textContent;
+    menu.textContent = selectedCategory;
+    list.style.display="none";
+    
+}
+const api_url = `https://api.api-ninjas.com/v1/quotes?category=${selectedCategory}`;
 
 async function getquote() {
     const response = await fetch(api_url,{
@@ -11,16 +25,24 @@ async function getquote() {
             'x-api-key':'zIOuSUxHltGN70PDb+CLaA==AUBMndoU4jvA4z0i'
         }
     });
-    var data = await response.json();
-    console.log(data);
-    quotes.innerHTML=data[0].quote;
-    authors.innerHTML=data[0].author;
+    if(response.ok){
+        const data = await response.json();
+        console.log(data);
+        quotes.innerHTML=data[0].quote;
+        authors.innerHTML=data[0].author;
+    }else{
+        quotes.innerHTML = "No quotes found for this category.";
+        authors.innerHTML = "";
+    }
+
 
 }
 getquote();
 
-function tweets(){
-    window.open("https://twitter.com/intent/tweet?text=" + quotes.innerHTML + "--- by"+ authors.innerHTML+"tweet window","width=600,height=300")
+function tweets() {
+    window.open("https://twitter.com/intent/tweet?text=" + quotes.innerHTML + " --- by " + authors.innerHTML, "tweet window", "width=600,height=300");
 }
-tweet.addEventListener("click",tweets)
-newBtn.addEventListener("click",getquote)
+tweet.addEventListener("click",tweets);
+newBtn.addEventListener("click",getquote);
+menu.addEventListener("click",menuItem);
+list.addEventListener("click",listItem)
