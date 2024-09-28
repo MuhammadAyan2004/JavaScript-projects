2239ada0ba44ea734efc8d53c30bd5d6d42c8d1b
Muhammad AYAN <m101ayan92@gmail.com>
2024-09-29 02:01:25 +0500

    Update script.js

--- daily quote/script.js
+++ daily quote/script.js
@@ -5,17 +5,14 @@ const tweet = document.getElementById("tweet");
 const menu = document.getElementById("menu");
 const list = document.getElementById("list");
 let selectedCategory = '';
-
 function menuItem(){
     list.style.display="block"; 
 }
-
 function listItem(e){
     selectedCategory = e.target.textContent;
     menu.textContent = selectedCategory;
     list.style.display="none";
 }
-
 const api_url = `https://api.api-ninjas.com/v1/quotes?category=${selectedCategory}`;
 async function getquote() {
     const response = await fetch(api_url,{
@@ -23,7 +20,6 @@ async function getquote() {
         headers:{
             'x-api-key':'zIOuSUxHltGN70PDb+CLaA==AUBMndoU4jvA4z0i'
         }
-
     });
     if(response.ok){
         const data = await response.json();
@@ -35,12 +31,10 @@ async function getquote() {
         authors.innerHTML = "";
     }
 }
-
 getquote();
 function tweets() {
     window.open("https://twitter.com/intent/tweet?text=" + quotes.innerHTML + " --- by " + authors.innerHTML, "tweet window", "width=600,height=300");
 }
-
 tweet.addEventListener("click",tweets);
 newBtn.addEventListener("click",getquote);
 menu.addEventListener("click",menuItem);
