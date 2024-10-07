197f4496c3a248806a34c1a847f3d2ea236bd32c
Muhammad AYAN <m101ayan92@gmail.com>
2024-10-07 16:39:31 +0500

    Update script.js

--- Image Gallery/script.js
+++ Image Gallery/script.js
@@ -2,20 +2,19 @@ let scrollContainer = document.querySelector(".container");
 let nextbtn = document.getElementById("nextbtn");
 let backbtn = document.getElementById("backbtn");
 
-scrollContainer.addEventListener("wheel",(evt)=>{
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-    
+scrollContainer.addEventListener("wheel",(e)=>{
+    e.preventDefault();
+    scrollContainer.scrollLeft += e.deltaX;
     scrollContainer.style.scrollBehavior = "auto";
 })
 
 nextbtn.addEventListener("click",()=>{
     
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 900;
+    scrollContainer.scrollLeft += 1000;
 })
 
 backbtn.addEventListener("click",()=>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 900;
+    scrollContainer.scrollLeft -= 1000;
 })
