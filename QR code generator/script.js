ca6119f114748bde5cfb2c343470a241b1da830d
Muhammad AYAN <m101ayan92@gmail.com>
2024-10-07 16:39:21 +0500

    Update script.js

--- QR code generator/script.js
+++ QR code generator/script.js
@@ -8,13 +8,10 @@ function codeGenerator (){
         qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input.value;
         imgBox.classList.add("show-img");
     }else{
-        input.classList.add("error")
+        input.classList.add("error");
         setTimeout(()=>{
             input.classList.remove("error")
         },1000)
     }
-
 }
-
-
 btn.addEventListener("click",codeGenerator)
