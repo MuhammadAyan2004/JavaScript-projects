8cac18fffbd2c129f0be3c2a474489210fb65943
Muhammad AYAN <m101ayan92@gmail.com>
2024-10-07 16:38:43 +0500

    Update script.js

--- Drag and drop/script.js
+++ Drag and drop/script.js
@@ -2,8 +2,8 @@ let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
-for(list of lists){
-    list.addEventListener("dragstart",function(e){
+for(box of lists){
+    box.addEventListener("dragstart",function(e){
         let selected = e.target;
         rightBox.addEventListener("dragover",function(e){
             e.preventDefault();
