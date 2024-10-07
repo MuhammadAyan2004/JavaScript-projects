91b3b89ffd2f67c41177e5a8ed7417b88203fa75
Muhammad AYAN <m101ayan92@gmail.com>
2024-10-07 16:38:51 +0500

    Update script.js

--- toast notification/script.js
+++ toast notification/script.js
@@ -2,24 +2,23 @@ let toastBox = document.getElementById("toastBox");
 const buttons = document.querySelectorAll(".btn");
 
 function showToast(e){
-    let toast= document.createElement("div")
+
+    let toast = document.createElement("div");
     toast.classList.add("toast");
 
     const msg = e.target.getAttribute("data-message");
-    
     const toastType = e.target.classList[1];
-    toast.classList.add(toastType);
-
     toast.innerHTML = msg;
-    toastBox.appendChild(toast);
+    toast.classList.add(toastType);
+    toastBox.appendChild(toast)
 
     setTimeout(()=>{
-        toast.remove("toast");
-    },5000);
+        toastBox.remove("toast")
+    },5000)
 
 
 }
-buttons.forEach(button=>{
+
+buttons.forEach(button =>{
     button.addEventListener("click",showToast);
 })
-
